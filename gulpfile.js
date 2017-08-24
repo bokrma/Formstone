5c574400aae6f316021f53581b5ae33a0717c6ae
Ben Plum <mr@benplum.com>
2017-08-24 11:12:26 -0400

    Testing less to sass

--- gulpfile.js
+++ gulpfile.js
@@ -22,8 +22,8 @@ var buildDocs      = require('./tasks/docs.js');
 
 var lessImportNPM  = require('less-plugin-npm-import');
 
-// var sass           = require('gulp-sass');
-// var lessToSass     = require('gulp-less-to-scss');
+var sass           = require('gulp-sass');
+var lessToSass     = require('gulp-less-to-scss');
 
 // Vars
 var pkg = require('./package.json');
@@ -210,20 +210,20 @@ gulp.task('beautifyStyles', function() {
   //   .pipe(gulp.dest('./src/css/'));
 });
 
-// // Less to Sass
-//
-// gulp.task('lessToSass', function() {
-//   return gulp.src('./src/less/**/*.less')
-//     .pipe(lessToSass())
-//     .pipe(gulp.dest('./src/scss'));
-// });
-// gulp.task('sass', function () {
-//   return gulp.src('./src/scss/**/*.scss')
-//     .pipe(sass().on('error', sass.logError))
-//     .pipe(gulp.dest('./scss'));
-// });
-//
-// gulp.task('testSass', ['lessToSass', 'sass']);
+// Less to Sass
+
+gulp.task('lessToSass', function() {
+  return gulp.src('./src/less/**/*.less')
+    .pipe(lessToSass())
+    .pipe(gulp.dest('./src/scss'));
+});
+gulp.task('sass', function () {
+  return gulp.src('./src/scss/**/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./scss'));
+});
+
+gulp.task('testSass', ['lessToSass', 'sass']);
 
 // Tasks
 
